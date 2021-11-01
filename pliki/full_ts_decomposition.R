a5c64672c77d66f6efe8976bf682c34f86dba276
kamillyczko <kamlyczko@gmail.com>
2021-11-01 19:22:56 +0100

    decompose()/stl()

---
+++ pliki/full_ts_decomposition.R
@@ -0,0 +1,28 @@
+decom_c_a <- decompose(confirmed, type = "additive")
+decom_c_m <- decompose(confirmed, type = "multiplicative")
+decom_d_a <- decompose(deaths, type = "additive")
+decom_d_m <- decompose(deaths, type = "multiplicative")
+
+text_size <- 10
+title_c <- "Dekompozycja szeregu czasowego liczby zaka¿eñ"
+c_plot_a <- autoplot(decom_c_a) +
+  labs(title = paste(title_c, " - model addytywny")) + xlab("numer tygodnia") +
+  theme(plot.title = element_text(size = text_size))
+c_plot_m <- autoplot(decom_c_m) +
+  labs(title = paste(title_c, " - model multiplikatywny")) + xlab("numer tygodnia") +
+  theme(plot.title = element_text(size = text_size))
+title_d <- "Dekompozycja szeregu czasowego liczby œmierci"
+d_plot_a <- autoplot(decom_d_a) +
+  labs(title = paste(title_d, " - model addytywny")) + xlab("numer tygodnia") +
+  theme(plot.title = element_text(size = text_size))
+d_plot_m <- autoplot(decom_d_m) +
+  labs(title = paste(title_d, " - model multiplikatywny")) + xlab("numer tygodnia") +
+  theme(plot.title = element_text(size = text_size))
+
+plots_c <- list(c_plot_a, c_plot_m)
+plots_d <- list(d_plot_a, d_plot_m)
+grid.arrange(grobs = plots_c)
+grid.arrange(grobs = plots_d)
+
+
+rm(c_plot_a, d_plot_a, c_plot_m, d_plot_m, decom_c_a, decom_c_m, decom_d_a, decom_d_m, title_c, title_d, plots_c, plots_d, text_size)
---
+++ pliki/stl_decom.R
@@ -0,0 +1,22 @@
+log_confirmed <- log(confirmed)
+log_confirmed[is.infinite(log_confirmed)] <- 0
+log_deaths <- log(deaths)
+log_deaths[is.infinite(log_deaths)] <- 0
+decom_c <- stl(log_confirmed, s.window = 7)
+decom_d <- stl(log_deaths, s.window = 7)
+conf_m_ts <- cbind(confirmed,
+                   exp(decom_c$time.series[, 2]),
+                   exp(decom_c$time.series[, 1]),
+                   exp(decom_c$time.series[, 3]))
+deaths_m_ts <- cbind(deaths,
+                   exp(decom_d$time.series[, 2]),
+                   exp(decom_d$time.series[, 1]),
+                   exp(decom_d$time.series[, 3]))
+col_names <- c("data", "trend", "seasonal", "remainder")
+colnames(conf_m_ts) <- col_names
+colnames(deaths_m_ts) <- col_names
+plot.ts(conf_m_ts, main = "Dekompozycja szeregu czasowego liczby zaka¿eñ", xlab = "tydzieñ")
+plot.ts(deaths_m_ts, main = "Dekompozycja szeregu czasowego liczby œmierci", xlab = "tydzieñ")
+rm(log_confirmed, log_deaths, decom_c, decom_d, conf_m_ts, deaths_m_ts, col_names )
+
+
