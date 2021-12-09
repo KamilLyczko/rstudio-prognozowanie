69ed92a28bea04da7a1d140e07b95e37f3486c23
kamillyczko <kamlyczko@gmail.com>
2021-12-09 16:51:57 +0100

    Update common_functions.R

--- project/scripts/common_functions.R
+++ project/scripts/common_functions.R
@@ -1,20 +1,4 @@
-day_of_year_number <- function(date) {
-  date <- as.Date(date)
-  month_number <- as.numeric(format(date, "%m"))
-  day_number <- as.numeric(format(date, "%d"))
-  year_number <- as.numeric(format(date, "%y"))
-  month_days <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30);
-  day_in_year <- day_number
-  for (i in 1:month_number) {
-    if(i==month_number)
-      break
-    day_in_year <- day_in_year + month_days[i]
-  }
-  if (!(year_number %% 4) && month_number > 2)
-    day_in_year <- day_in_year + 1
-  return(day_in_year)
-}
-
+#funkcja zwracająca wektor opisujący czas obserwacji o podanym numerze
 weekly_freq_day_number <- function(obs_num) {
   offset <- c(1, 5)
   week = (obs_num %/% 7) + 1
@@ -25,7 +9,6 @@ weekly_freq_day_number <- function(obs_num) {
   }
   return(c(week, day))
 }
-
 
 #funkcja zwracająca numer obserwacji dla podanego numeru tygodnia i dnia
 #argument jako wektor dwóch liczb (numer tygodnia, numer dnia)
@@ -74,68 +57,8 @@ get_index_of_obs <- function(ts, obs_num) {
 get_numb_of_obs_ts <- function(ts, index) {
   return(calc_obs_num(calc_obs_week(index)))
 }
- 
 
-#funkcja obliczająca błędy ex post dla podanych prognoz i wartości testowych
-calculate_ex_post_errors <- function(forecast, test_ts) {
-  n <- length(test_ts)
-  j <- 0
-  errors <- data.frame(
-    ME = 0, MAE = 0, MSE = 0, RMSE = 0, MAPE = 0, row.names = "value"
-  )
-  for(i in 1:n) {
-    e <- test_ts[i] - forecast$mean[i]
-    errors["ME"] <- errors["ME"] + e
-    errors["MAE"] <- errors["MAE"] + abs(e)
-    errors["MSE"] <- errors["MSE"] + e*e
-    if(test_ts[i] != 0) {
-      errors["MAPE"] <- errors["MAPE"] + abs(e/test_ts[i])
-      j <- j + 1
-    }
-  }
-  errors["ME"] <- errors["ME"]/n
-  errors["MAE"] <- errors["MAE"]/n
-  errors["MSE"] <- errors["MSE"]/n
-  errors["RMSE"] <- sqrt(errors["MSE"])
-  errors["MAPE"] <- errors["MAPE"]/j*100
-  return(errors)
-}
 
-#funkcja zapisująca prognozy do pliku csv o podanej nazwie w podanym podkatalogu
-save_forecasts_to_csv <- function(forecast, file_name, subdir = "") {
-  intervals_list <- list()
-  for(i in 1:length(forecast$level)) {
-    intervals_list[[i]] <- data.frame(
-      forecast$lower[, i],
-      forecast$upper[, i]
-    )
-    names <- c(
-      paste("lower", forecast$level[i], "%", sep = ""),
-      paste("upper", forecast$level[i], "%", sep = "")
-      )
-    colnames(intervals_list[[i]]) <- names
-  }
-  start_date <- as.Date(get_date_of_obs_ts(get_index_of_obs(forecast$mean, 1)))
-  finish_date <- as.Date(get_date_of_obs_ts(get_index_of_obs(forecast$mean, length(forecast$mean))))
-  dates <- as.Date(c(seq(start_date, finish_date, 1)))
-  df <- data.frame(
-    h = c(1:length(forecast$mean)),
-    date = format(dates, "%d.%m.%Y"),
-    prediction = forecast$mean
-  )
-  if(stringi::stri_length(subdir) > 0)
-    subdir <- paste0(subdir, "/")
-  directory <- paste("data_sheets/", subdir, file_name, sep = "")
-  write.csv2(cbind(df, intervals_list), directory, row.names = FALSE)
-}
-
-#funkcja zapisująca podaną ramkę danych do pliku csv o podanej nazwie
-save_df_to_csv <- function(df, file_name, subdir = "") {
-  if(stringi::stri_length(subdir) > 0)
-    subdir <- paste0(subdir, "/")
-  directory <- paste("data_sheets/", subdir, file_name, sep = "")
-  write.csv2(df, directory, row.names = FALSE)
-}
 
 #funkcja nadająca wartości obserwacjom bez wartości
 replace_na_values <- function(ts){
@@ -150,7 +73,75 @@ replace_na_values <- function(ts){
   return(ts)
 }
 
+#funkcja zwracająca wektor indeksów obserwacji równych 0
+get_indexes_of_zero_values <- function(ts) {
+  j <- 0
+  indexes <- c()
+  for(i in 1:length(ts)) {
+    if(ts[i] == 0) {
+      j <- j + 1
+      indexes[j] <- i
+    }
+  }
+  return(indexes)
+}
 
+
+#funkcje do generowania wykresów-------------------------------------------------------------
+
+#funkcja generująca wykres z prognozami z numerami tygodni na osi czasu
+generate_forecast_plot <- function(forecast, title = "", ylab = "", PI = TRUE) {
+  autoplot(forecast$x, series = "Wartości rzeczywiste") + 
+    autolayer(forecast, series = "Prognozy", PI = PI) +
+    autolayer(fitted(forecast), series = "Wartości dopasowane") +
+    coord_cartesian(ylim = c(0, max(forecast$x, forecast$mean, forecast$fitted))) +
+    guides(colour = guide_legend(title = "")) +
+    labs(title = title, x = "numer tygodnia", y = ylab, color = "")
+}
+
+#funkcja generująca wykres z prognozami z datami na osi czasu
+generate_forecast_plot2 <- function(forecast, title = "", ylab = "") {
+  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(forecast$x, 1)),
+                         get_date_of_obs_ts(get_index_of_obs(forecast$mean, 
+                                                             length(forecast$mean))), 1)))
+  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
+  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
+    x_labs[length(x_labs) + 1] <- dates[length(dates)]
+  x_ts <- ts(data = c(forecast$x, rep(NA, length(forecast$mean))),
+             start = start(forecast$x), frequency = frequency(forecast$x))
+  fitted_ts <- ts(data = c(forecast$fitted, rep(NA, length(forecast$mean))),
+                  start = start(forecast$fitted), 
+                  frequency = frequency(forecast$fitted))
+  prediction_ts <- ts(data = c(rep(NA, length(forecast$x)), forecast$mean),
+                      start = start(forecast$x), frequency = frequency(forecast$mean))
+  ggplot(data = data.frame(x = dates, ts = x_ts, fit = fitted_ts,
+                           prediction = prediction_ts)) +
+    geom_line(aes(x, ts, colour = "Wartości rzeczywiste")) +
+    geom_line(aes(x, fit, colour = "Wartości dopasowane")) +
+    geom_line(aes(x, prediction, colour = "Prognozy")) +
+    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
+    labs(title = title, x = "data", y = ylab, color = "") +
+    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
+}
+
+#funkcja generująca wykres z wartościami rzeczywistymi 
+# i wartościami dopasowanymi na podstawie podanego modelu
+generate_fit_plot <- function(fit, title = "", ylab = "") {
+  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(fit$x, 1)),
+                         get_date_of_obs_ts(get_index_of_obs(fit$x, length(fit$x))), 1)))
+  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
+  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
+    x_labs[length(x_labs) + 1] <- dates[length(dates)]
+  ggplot(data = data.frame(x = dates, y = fit$x), aes(x, y, colour = "Wartości rzeczywiste")) +
+    geom_line() +
+    geom_line(data = data.frame(x = dates, y = fitted(fit)), 
+              aes(x, y, colour = "Wartości dopasowane")) +
+    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
+    labs(title = title, x = "data", y = ylab, color = "") +
+    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
+}
+
+#funkcja generująca wykres porównania wartości rzeczywistych z prognozami
 generate_test_comparison_plot <- function(test_ts, forecast_object, title = "", ylab = "") {
   dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(test_ts, 1)),
                          get_date_of_obs_ts(get_index_of_obs(test_ts, length(test_ts))), 1)))
@@ -166,19 +157,30 @@ generate_test_comparison_plot <- function(test_ts, forecast_object, title = "", 
     theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
 }
 
-get_indexes_of_zero_values <- function(ts) {
-  j <- 0
-  indexes <- c()
-  for(i in 1:length(ts)) {
-    if(ts[i] == 0) {
-      j <- j + 1
-      indexes[j] <- i
-    }
-  }
-  return(indexes)
+#funkcja generująca wykres czasowy podanego szeregu
+generate_ts_time_plot <- function(ts, title = "", ylab = "") {
+  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(ts, 1)),
+                         get_date_of_obs_ts(get_index_of_obs(ts, length(ts))), 1)))
+  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
+  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
+    x_labs[length(x_labs) + 1] <- dates[length(dates)]
+  ggplot(data = data.frame(x = dates, y = ts), aes(x = x, y = y)) +
+    geom_line() +
+    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
+    labs(title = title, x = "data", y = ylab) +
+    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
 }
 
+#funkcja do ustawiania rozmiaru głównego tytułu oraz tytułów osi wykresu
+set_titles_size <- function(main = 10, axis = 10) {
+  theme(axis.title.x = element_text(size = axis), 
+        axis.title.y = element_text(size = axis),
+        plot.title = element_text(size = main))
+}
 
+#funkcje operujące na miernikach błędów predykcji-------------------------------------
+
+#funkcja obliczająca błędy treningowe
 calculate_training_errors <- function(fit) {
   train_errors <- residuals(fit, type = "response")
   n <- length(train_errors)
@@ -208,55 +210,29 @@ calculate_training_errors <- function(fit) {
   return(errors)
 }
 
-generate_fit_plot <- function(fit, title = "", ylab = "") {
-  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(fit$x, 1)),
-                         get_date_of_obs_ts(get_index_of_obs(fit$x, length(fit$x))), 1)))
-  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
-  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
-    x_labs[length(x_labs) + 1] <- dates[length(dates)]
-  ggplot(data = data.frame(x = dates, y = fit$x), aes(x, y, colour = "Wartości rzeczywiste")) +
-    geom_line() +
-    geom_line(data = data.frame(x = dates, y = fitted(fit)), 
-              aes(x, y, colour = "Wartości dopasowane")) +
-    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
-    labs(title = title, x = "data", y = ylab, color = "") +
-    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
-}
-
-generate_forecast_plot <- function(forecast, title = "", ylab = "", PI = TRUE) {
-  autoplot(forecast$x, series = "Wartości rzeczywiste") + 
-    autolayer(forecast, series = "Prognozy", PI = PI) +
-    autolayer(fitted(forecast), series = "Wartości dopasowane") +
-    coord_cartesian(ylim = c(0, max(forecast$x, forecast$mean, forecast$fitted))) +
-    guides(colour = guide_legend(title = "")) +
-    labs(title = title, x = "numer tygodnia", y = ylab, color = "")
-}
-
-generate_forecast_plot2 <- function(forecast, title = "", ylab = "") {
-  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(forecast$x, 1)),
-                         get_date_of_obs_ts(get_index_of_obs(forecast$mean, 
-                                                             length(forecast$mean))), 1)))
-  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
-  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
-    x_labs[length(x_labs) + 1] <- dates[length(dates)]
-  n <- length(forecast$x)
-  point <- forecast$x
-  point[] <- NA
-  x_ts <- ts(data = c(forecast$x, rep(NA, length(forecast$mean))),
-             start = start(forecast$x), frequency = frequency(forecast$x))
-  fitted_ts <- ts(data = c(forecast$fitted, rep(NA, length(forecast$mean))),
-                  start = start(forecast$fitted), 
-                  frequency = frequency(forecast$fitted))
-  prediction_ts <- ts(data = c(rep(NA, length(forecast$x)), forecast$mean),
-                      start = start(forecast$x), frequency = frequency(forecast$mean))
-  ggplot(data = data.frame(x = dates, ts = x_ts, fit = fitted_ts,
-                           prediction = prediction_ts)) +
-    geom_line(aes(x, ts, colour = "Wartości rzeczywiste")) +
-    geom_line(aes(x, fit, colour = "Wartości dopasowane")) +
-    geom_line(aes(x, prediction, colour = "Prognozy")) +
-    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
-    labs(title = title, x = "data", y = ylab, color = "") +
-    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
+#funkcja obliczająca błędy ex post dla podanych prognoz i wartości testowych
+calculate_ex_post_errors <- function(forecast, test_ts) {
+  n <- length(test_ts)
+  j <- 0
+  errors <- data.frame(
+    ME = 0, MAE = 0, MSE = 0, RMSE = 0, MAPE = 0, row.names = "value"
+  )
+  for(i in 1:n) {
+    e <- test_ts[i] - forecast$mean[i]
+    errors["ME"] <- errors["ME"] + e
+    errors["MAE"] <- errors["MAE"] + abs(e)
+    errors["MSE"] <- errors["MSE"] + e*e
+    if(test_ts[i] != 0) {
+      errors["MAPE"] <- errors["MAPE"] + abs(e/test_ts[i])
+      j <- j + 1
+    }
+  }
+  errors["ME"] <- errors["ME"]/n
+  errors["MAE"] <- errors["MAE"]/n
+  errors["MSE"] <- errors["MSE"]/n
+  errors["RMSE"] <- sqrt(errors["MSE"])
+  errors["MAPE"] <- errors["MAPE"]/j*100
+  return(errors)
 }
 
 #funkcja zwracająca indeks modelu w podanej liście,
@@ -319,23 +295,45 @@ find_best_forecast <- function(forecasts_list, test_ts) {
   return(match(max(forecasts_points), forecasts_points))
 }
 
-generate_ts_time_plot <- function(ts, title = "", ylab = "") {
-  dates <- as.Date(c(seq(get_date_of_obs_ts(get_index_of_obs(ts, 1)),
-                         get_date_of_obs_ts(get_index_of_obs(ts, length(ts))), 1)))
-  x_labs <- c(seq(dates[1], dates[length(dates)], length(dates)%/%10))
-  if(as.numeric(dates[length(dates)] - x_labs[length(x_labs)]) > 10)
-    x_labs[length(x_labs) + 1] <- dates[length(dates)]
-  ggplot(data = data.frame(x = dates, y = ts), aes(x = x, y = y)) +
-    geom_line() +
-    scale_x_date(breaks = x_labs, labels = x_labs, date_labels = "%d-%m-%Y") +
-    labs(title = title, x = "data", y = ylab) +
-    theme(axis.text.x = element_markdown(angle = 45, hjust = 1))
+#funkcje zapisu danych do pliku-------------------------------------------------
+
+#funkcja zapisująca prognozy do pliku csv o podanej nazwie w podanym podkatalogu
+save_forecasts_to_csv <- function(forecast, file_name, subdir = "") {
+  intervals_list <- list()
+  for(i in 1:length(forecast$level)) {
+    intervals_list[[i]] <- data.frame(
+      forecast$lower[, i],
+      forecast$upper[, i]
+    )
+    names <- c(
+      paste("lower", forecast$level[i], "%", sep = ""),
+      paste("upper", forecast$level[i], "%", sep = "")
+    )
+    colnames(intervals_list[[i]]) <- names
+  }
+  start_date <- as.Date(get_date_of_obs_ts(get_index_of_obs(forecast$mean, 1)))
+  finish_date <- as.Date(get_date_of_obs_ts(get_index_of_obs(forecast$mean, length(forecast$mean))))
+  dates <- as.Date(c(seq(start_date, finish_date, 1)))
+  df <- data.frame(
+    h = c(1:length(forecast$mean)),
+    date = format(dates, "%d.%m.%Y"),
+    prediction = forecast$mean
+  )
+  if(stringi::stri_length(subdir) > 0)
+    subdir <- paste0(subdir, "/")
+  directory <- paste("data_sheets/", subdir, file_name, sep = "")
+  write.csv2(cbind(df, intervals_list), directory, row.names = FALSE)
 }
 
-#funkcja do ustawiania rozmiaru głównego tytułu oraz tytułów osi wykresu
-set_titles_size <- function(main = 10, axis = 10) {
-  theme(axis.title.x = element_text(size = axis), 
-        axis.title.y = element_text(size = axis),
-        plot.title = element_text(size = main))
+#funkcja zapisująca podaną ramkę danych do pliku csv o podanej nazwie
+save_df_to_csv <- function(df, file_name, subdir = "") {
+  if(stringi::stri_length(subdir) > 0)
+    subdir <- paste0(subdir, "/")
+  directory <- paste("data_sheets/", subdir, file_name, sep = "")
+  write.csv2(df, directory, row.names = FALSE)
 }
+
+
+
+
 
