28d3b28f8a4cb71bbe6492f54d5d4e396b78558c
kamillyczko <kamlyczko@gmail.com>
2021-12-19 13:58:46 +0100

    Update best_arima_models.R

--- project/scripts/statistica_arima/best_arima_models.R
+++ project/scripts/statistica_arima/best_arima_models.R
@@ -58,4 +58,35 @@ model <- auto.arima(train, lambda = 0)
 summary(model) #ARIMA(3,1,3)(2,1,2)[7] 
 autoplot(forecast(model, h = 30), PI = FALSE) + autolayer(fitted(model))
 
+#===============================================================================
+c_models <- list()
+d_models <- list()
+for(i in 1:4){
+  c_train <- window(c_time_series[[i]], 
+                    end = weekly_freq_day_number(
+                      length(c_time_series[[i]]) - 30)) + 1
+  d_train <- window(d_time_series[[i]], 
+                    start = weekly_freq_day_number(24),
+                    end = weekly_freq_day_number(
+                      length(d_time_series[[i]]) - 30)) + 1
+  c_models[[i]] <- auto.arima(c_train, lambda = 0)
+  d_models[[i]] <- auto.arima(d_train, lambda = 0)
+}
+
+mes <- 
+  "Wybrane modele:\n\nSzeregi liczb zakażeń:\n"
+for(i in 1:length(c_models)){
+  mes <- paste0(mes, i, " fala:\t",
+                get_arima_model_type(
+                  c_models[[i]]), "\n")
+}
+mes <- paste0(mes, "\nSzeregi liczb śmierci:\n")
+for(i in 1:length(d_models)){
+  mes <- paste0(mes, i, " fala:\t",
+                get_arima_model_type(
+                  d_models[[i]]), "\n")
+}
+cat(mes)
+
 rm(train, model)
+rm(c_models, d_models, c_train, d_train, i, mes)
